4d3bd967e98152b03495a79b922fbf358fde02ce
NelsonZeng25 <nelson.zeng25@gmail.com>
2022-02-11 14:53:57 -0500

    Added Dining Philosophers with deadlock and formatted code with GoogleStyle

--- src/ca/mcgill/ecse420/a1/DiningPhilosophers.java
+++ src/ca/mcgill/ecse420/a1/DiningPhilosophers.java
@@ -2,23 +2,61 @@ package ca.mcgill.ecse420.a1;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.locks.ReentrantLock;
 
 public class DiningPhilosophers {
 
     public static void main(String[] args) {
-
         int numberOfPhilosophers = 5;
         Philosopher[] philosophers = new Philosopher[numberOfPhilosophers];
         Object[] chopsticks = new Object[numberOfPhilosophers];
+
+        ExecutorService executor = Executors.newFixedThreadPool(numberOfPhilosophers);
+
+        for (int i = 0; i < numberOfPhilosophers; i++) {
+            chopsticks[i] = new Object();
+        }
+
+        for (int i = 0; i < numberOfPhilosophers; i++) {
+            philosophers[i] = new Philosopher(i, chopsticks[i], chopsticks[(i + 1) % numberOfPhilosophers]);
+            executor.execute(philosophers[i]);
+        }
+        executor.shutdown();
+        while (!executor.isTerminated());
     }
 
     public static class Philosopher implements Runnable {
+        int i;
+        final Object leftChopstick;
+        final Object rightChopstick;
 
+        public Philosopher(int i, Object leftChopstick, Object rightChopstick) {
+            this.i = i;
+            this.leftChopstick = leftChopstick;
+            this.rightChopstick = rightChopstick;
+        }
 
+        private void doAction(String action) throws InterruptedException {
+            System.out.println("Philosopher " + (i+1) + " " + action);
+            Thread.sleep(((int) (Math.random() * 100)));
+        }
 
         @Override public void run() {
-
+            try {
+                while (true) {
+                    doAction("thinking");
+                    synchronized (leftChopstick) {
+                        doAction("picked up left chopstick");
+                        synchronized (rightChopstick) {
+                            doAction("picked up right chopstick");
+                            doAction("eating");
+                        }
+                        doAction("put down right chopstick");
+                    }
+                    doAction("picked up left chopstick");
+                }
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
         }
 
 
--- src/ca/mcgill/ecse420/a1/MatrixMultiplication.java
+++ src/ca/mcgill/ecse420/a1/MatrixMultiplication.java
@@ -4,91 +4,94 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class MatrixMultiplication {
-	
-	private static int NUMBER_THREADS = 7;
-	private static final int MATRIX_SIZE = 2000;
+
+    private static int NUMBER_THREADS = 7;
+    private static final int MATRIX_SIZE = 2000;
 
     public static void main(String[] args) {
-		/*
-		// Question 1.4
-		double[][] a = generateRandomMatrix(MATRIX_SIZE, MATRIX_SIZE);
-		double[][] b = generateRandomMatrix(MATRIX_SIZE, MATRIX_SIZE);
-		
-		for (int i = 1 ; i <= 10 ; i++) {
-			NUMBER_THREADS = i;
-			measureExecutionTime(a, b, "parallel");
-		}
-		*/
-		
-		// Question 1.5:
-		int[] matrixSizes = new int[] {100,200,500,1000,2000,3000,4000};
-		for (int size : matrixSizes) {
-			double[][] matrixA = generateRandomMatrix(size, size);
-			double[][] matrixB = generateRandomMatrix(size, size);
-			measureExecutionTime(matrixA, matrixB, "parallel");
-		}
+        /*
+        // Question 1.4
+        double[][] a = generateRandomMatrix(MATRIX_SIZE, MATRIX_SIZE);
+        double[][] b = generateRandomMatrix(MATRIX_SIZE, MATRIX_SIZE);
+
+        for (int i = 1 ; i <= 10 ; i++) {
+          NUMBER_THREADS = i;
+          measureExecutionTime(a, b, "parallel");
+        }
+        */
+
+        // Question 1.5:
+        int[] matrixSizes = new int[] {100, 200, 500, 1000, 2000, 3000, 4000};
+        for (int size : matrixSizes) {
+            double[][] matrixA = generateRandomMatrix(size, size);
+            double[][] matrixB = generateRandomMatrix(size, size);
+            measureExecutionTime(matrixA, matrixB, "parallel");
+        }
     }
-	
-	/**
-	 * Returns the result of a sequential matrix multiplication
-	 * The two matrices are randomly generated
-	 * @param a is the first matrix
-	 * @param b is the second matrix
-	 * @return the result of the multiplication
-	 * */
-	public static double[][] sequentialMultiplyMatrix(double[][] a, double[][] b) {
-		
-		double tempResult = 0;
-		int rows = b.length;
-		int cols = a[0].length;
-		
-		double[][] result = new double[rows][cols];
-		
-		for (int i = 0 ; i < rows ; i++) {
-			for (int j = 0 ; j < rows ; j++) {
-				for (int k = 0 ; k < cols ; k++) {
-					tempResult += a[i][k] * b[k][j];
-				}
-				
-				result[i][j] = tempResult;
-				tempResult = 0;
-			}
-		}
-		
-		return result;
-	}
-	
-	/**
-	 * Returns the result of a concurrent matrix multiplication
-	 * The two matrices are randomly generated
-	 * @param a is the first matrix
-	 * @param b is the second matrix
-	 * @return the result of the multiplication
-	 * */
+
+    /**
+     * Returns the result of a sequential matrix multiplication
+     * The two matrices are randomly generated
+     *
+     * @param a is the first matrix
+     * @param b is the second matrix
+     * @return the result of the multiplication
+     */
+    public static double[][] sequentialMultiplyMatrix(double[][] a, double[][] b) {
+
+        double tempResult = 0;
+        int rows = b.length;
+        int cols = a[0].length;
+
+        double[][] result = new double[rows][cols];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < rows; j++) {
+                for (int k = 0; k < cols; k++) {
+                    tempResult += a[i][k] * b[k][j];
+                }
+
+                result[i][j] = tempResult;
+                tempResult = 0;
+            }
+        }
+
+        return result;
+    }
+
+    /**
+     * Returns the result of a concurrent matrix multiplication
+     * The two matrices are randomly generated
+     *
+     * @param a is the first matrix
+     * @param b is the second matrix
+     * @return the result of the multiplication
+     */
     public static double[][] parallelMultiplyMatrix(double[][] a, double[][] b) {
-    
-    	double[][] result = new double[b.length][a[0].length];
-    	
-    	ExecutorService executor = Executors.newFixedThreadPool(NUMBER_THREADS);
-    	
-		for (int i = 0 ; i < b.length ; i++) {
-			for (int j = 0 ; j < a[0].length ; j++) {
-				executor.execute((new MatrixMultiplicationParallel(a,b,result,i,j,a.length)));
-			}
-		}
-		
-		executor.shutdown();
-		
-		// Ensure executor terminates
-		while(!executor.isTerminated()) {}
-		
-		return result;
+
+        double[][] result = new double[b.length][a[0].length];
+
+        ExecutorService executor = Executors.newFixedThreadPool(NUMBER_THREADS);
+
+        for (int i = 0; i < b.length; i++) {
+            for (int j = 0; j < a[0].length; j++) {
+                executor.execute((new MatrixMultiplicationParallel(a, b, result, i, j, a.length)));
+            }
+        }
+
+        executor.shutdown();
+
+        // Ensure executor terminates
+        while (!executor.isTerminated()) {
+        }
+
+        return result;
     }
-    
+
     /**
      * Runnable class used for parallel matrix multiplication.
-     * @author arneetkalra
      *
+     * @author arneetkalra
      */
     public static class MatrixMultiplicationParallel implements Runnable {
 
@@ -100,7 +103,8 @@ public class MatrixMultiplication {
         private double[][] matrixB;
         private double[][] matrixC;
 
-        public MatrixMultiplicationParallel(double[][] matrixA, double[][] matrixB, double[][] matrixC, int row, int column, int size) {
+        public MatrixMultiplicationParallel(double[][] matrixA, double[][] matrixB,
+            double[][] matrixC, int row, int column, int size) {
             this.row = row;
             this.column = column;
             this.size = size;
@@ -118,48 +122,49 @@ public class MatrixMultiplication {
             matrixC[row][column] = result;
         }
     }
-        
+
     /**
      * Populates a matrix of given size with randomly generated integers between 0-10.
+     *
      * @param numRows number of rows
      * @param numCols number of cols
      * @return matrix
      */
-    private static double[][] generateRandomMatrix (int numRows, int numCols) {
+    private static double[][] generateRandomMatrix(int numRows, int numCols) {
         double matrix[][] = new double[numRows][numCols];
-        for (int row = 0 ; row < numRows ; row++ ) {
-            for (int col = 0 ; col < numCols ; col++ ) {
+        for (int row = 0; row < numRows; row++) {
+            for (int col = 0; col < numCols; col++) {
                 matrix[row][col] = (double) ((int) (Math.random() * 10.0));
             }
         }
         return matrix;
     }
-    
+
     /**
      * Measures the execution time of multiplying matricies.
-     *  
+     *
      * @param matrixA first matrix to multiply
      * @param matrixB other matrix to multiply
-     * @param type can take in 3 values:
-     * 		"parallel" for only parallel execution,
-     * 		"sequential" for only sequential execution
-     *      "both" for both types of execution 
+     * @param type    can take in 3 values:
+     *                "parallel" for only parallel execution,
+     *                "sequential" for only sequential execution
+     *                "both" for both types of execution
      */
-    public static void measureExecutionTime (double[][] matrixA, double[][] matrixB, String type) {
-    	if (type.equals("both") || type.equals("sequential")) {
-    		long sequentialStart = System.currentTimeMillis();
-        	sequentialMultiplyMatrix(matrixA, matrixB);
-        	long sequentialStop = System.currentTimeMillis();
-        	System.out.println(sequentialStop - sequentialStart);
-    	}
-    	
-    	if (type.equals("both") || type.equals("parallel")) {
-	    	long parallelStart = System.currentTimeMillis();
-	    	parallelMultiplyMatrix(matrixA, matrixB);
-	    	long parallelStop = System.currentTimeMillis();
-	    	System.out.println(parallelStop - parallelStart);
-    	}
-    } 
+    public static void measureExecutionTime(double[][] matrixA, double[][] matrixB, String type) {
+        if (type.equals("both") || type.equals("sequential")) {
+            long sequentialStart = System.currentTimeMillis();
+            sequentialMultiplyMatrix(matrixA, matrixB);
+            long sequentialStop = System.currentTimeMillis();
+            System.out.println(sequentialStop - sequentialStart);
+        }
+
+        if (type.equals("both") || type.equals("parallel")) {
+            long parallelStart = System.currentTimeMillis();
+            parallelMultiplyMatrix(matrixA, matrixB);
+            long parallelStop = System.currentTimeMillis();
+            System.out.println(parallelStop - parallelStart);
+        }
+    }
 }
 
 
